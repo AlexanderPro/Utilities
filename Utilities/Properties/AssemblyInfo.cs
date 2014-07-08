e469a713a0365d247ac61d1d526fdb322ff0963c
AlexanderPro <illarionov.pro@gmail.com>
2014-07-08 15:00:31 +0400

    Changed an assembly description attribute and nuspec file.

--- Utilities/Properties/AssemblyInfo.cs
+++ Utilities/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Utilities")]
-[assembly: AssemblyDescription("This .net library contains many useful methods, extensions, helper classes, etc.")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alexander Illarionov")]
 [assembly: AssemblyProduct("Utilities")]
 [assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
