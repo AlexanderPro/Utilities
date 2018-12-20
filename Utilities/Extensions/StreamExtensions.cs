51506d5d9c697fbb3ac50af4d9b6031567f4bb19
AlexanderPro <illarionov.pro@gmail.com>
2018-12-20 17:53:37 +0300

    Added StreamExtensions class.

---
+++ Utilities/Extensions/StreamExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Utilities.Extensions
+{
+    /// <summary>
+    /// Extension methods for Stream.
+    /// </summary>
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Read all bytes from a stream.
+        /// </summary>
+        /// <param name="stream">The instance of the <see cref="Stream"/> class.</param>
+        /// <returns>Byte array.</returns>
+        public static byte[] ReadAllBytes(this Stream stream)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
