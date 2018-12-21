99bffa9bd8b5e70a6fa9c8a27332fb0e52ceafac
AlexanderPro <illarionov.pro@gmail.com>
2018-12-21 16:27:17 +0300

    Added XmlExtensions class.

---
+++ Utilities/Extensions/XmlExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace Utilities.Extensions
+{
+    /// <summary>
+    /// Extension methods for Xml.
+    /// </summary>
+    public static class XmlExtensions
+    {
+        /// <summary>
+        /// Removes all namespaces from xml element.
+        /// </summary>
+        public static XElement RemoveAllNamespaces(this XElement e)
+        {
+            return new XElement(e.Name.LocalName,
+              (from n in e.Nodes()
+               select ((n is XElement) ? RemoveAllNamespaces(n as XElement) : n)),
+                  (e.HasAttributes) ?
+                    (from a in e.Attributes()
+                     where (!a.IsNamespaceDeclaration)
+                     select new XAttribute(a.Name.LocalName, a.Value)) : null);
+        }
+
+        /// <summary>
+        /// Gets an attribute's value from an xml node.
+        /// </summary>
+        public static string GetAttributeValueOrNull(this XmlNode node, string attributeName)
+        {
+            if (node.Attributes == null || node.Attributes.Count <= 0)
+            {
+                throw new Exception(node.Name + " node has not " + attributeName + " attribute");
+            }
+
+            return node.Attributes
+                .Cast<XmlAttribute>()
+                .Where(attr => attr.Name == attributeName)
+                .Select(attr => attr.Value)
+                .FirstOrDefault();
+        }
+    }
+}
