9a0200a70ce59d044fc8541355130863b229dd23
AlexanderPro <illarionov.pro@gmail.com>
2018-12-24 19:56:02 +0300

    Added DateTimeHelper class.

---
+++ Utilities/Common/DateTimeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities.Common
+{
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// Returns all days of the month.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <param name="month">The month.</param>
+        public static IEnumerable<DateTime> DaysOfMonth(int year, int month)
+        {
+            return Enumerable.Range(0, DateTime.DaysInMonth(year, month)).Select(day => new DateTime(year, month, day + 1));
+        }
+    }
+}
